55814d5e5e991a507e3215070151b4cfb8ec0b6c
dvankevich <dvankevich@gmail.com>
2025-12-11 17:31:12 +0200

    add clear search button

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -3,6 +3,10 @@ import css from "./SearchBox.module.css";
 
 const SearchBox = ({ search, handleSearch }) => {
   const searchInputId = useId();
+  const clearSearch = () => {
+    handleSearch("");
+  };
+
   return (
     <div className={css.box}>
       <label htmlFor={searchInputId} aria-label="Find contacts by name field">
@@ -14,6 +18,13 @@ const SearchBox = ({ search, handleSearch }) => {
         onChange={(evt) => handleSearch(evt.target.value)}
         value={search}
       ></input>
+      <button
+        className={css.btn}
+        onClick={clearSearch}
+        aria-label="Clear search button"
+      >
+        Clear
+      </button>
     </div>
   );
 };
